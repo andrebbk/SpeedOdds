1bad14a4ed7321db3f4b163e9004d8382f07b729
André Silva <andre.f.m.silva@hotmail.com>
2020-05-31 15:22:02 +0100

    Start window, app icon and background

---
+++ SpeedOdds/SpeedOdds/StartWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SpeedOdds.UserControls.Start;
+using System.Windows;
+
+namespace SpeedOdds
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+
+            //Show Start window
+            this.StartContainer.Content = new UserControl_StartWindow(this);            
+        }
+    }
+}
---
+++ SpeedOdds/SpeedOdds/UserControls/Start/UserControl_StartWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Controls;
+
+
+namespace SpeedOdds.UserControls.Start
+{
+    /// <summary>
+    /// Interaction logic for UserControl_StartWindow.xaml
+    /// </summary>
+    public partial class UserControl_StartWindow : UserControl
+    {
+        public UserControl_StartWindow(StartWindow startWindow)
+        {
+            InitializeComponent();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+
+                LabelWelcome.Dispatcher.BeginInvoke((Action)(
+                    () => LabelWelcome.Visibility = System.Windows.Visibility.Visible
+                ));
+
+                Thread.Sleep(1000);
+
+            }).Start();
+        }
+    }
+}
